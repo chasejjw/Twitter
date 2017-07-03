6b0e5ba0db95d59be8e89da03698c16a9c8bcaf3
Chase Warren <chasejjw@fb.com>
2017-07-03 14:37:13 -0700

    Created data models for Users and Tweets

--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -10,10 +10,19 @@ import Foundation
 
 class User {
     
-    var name: String
+    var name: String = ""
+    var screenName: String = ""
+    var profileBannerURL: String = ""
+    var profileImageURL: String = ""
+    var description: String = ""
+    static var current: User?
+    
     
     init(dictionary: [String: Any]) {
-        name = dictionary["name"] as! String
-
+        name = dictionary["name"] as? String ?? ""
+        screenName = dictionary["screen_name"] as? String ?? ""
+        profileBannerURL = dictionary["profile_banner_url"] as? String ?? ""
+        profileImageURL = dictionary["profile_image_url_https"] as? String ?? ""
+        description = dictionary["description"] as? String ?? ""
     }
 }
